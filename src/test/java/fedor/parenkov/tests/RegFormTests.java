3c6b5db033d8e14f70a693576d5b2257a7177d45
supersonic8005 <shivversky@gmail.com>
2021-09-05 13:21:48 +0400

    Update until datepeacker

---
+++ src/test/java/fedor/parenkov/tests/RegFormTests.java
@@ -0,0 +1,33 @@
+package fedor.parenkov.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byXpath;
+import static com.codeborne.selenide.Selenide.*;
+
+public class RegFormTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void RegFormTest() {
+        //String permanentAddress = "some street 1";
+
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Fedor");
+        $("#lastName").setValue("Parenkov");
+        $("#userEmail").setValue("parenkov.fedor@gmail.com");
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue("9065554433");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("May")).click();
+        $(".react-datepicker__year-select").$(byText("1986")).click();
+        $(byXpath("//div[@class='react-datepicker__day react-datepicker__day--001']")).click();
+    }
+}
